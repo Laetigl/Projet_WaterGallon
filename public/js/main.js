269d28059b8c8411c951fb785738862208502588
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-01-21 18:56:09 +0100

    Responsive, comments et last check

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 //Buttons + and -
 let allbuttonAdding = document.querySelectorAll(".add")
-console.log(allbuttonAdding);
 let allbuttonRemoving = document.querySelectorAll(".remove")
-console.log(allbuttonRemoving);
 
 //Content of jar
 let cupIngredient = 0
@@ -15,21 +13,14 @@ let minCup = 0;
 
 //Actual content
 let actualContentCup = document.querySelector(".actualPercent")
-// let actualContentMl = document.querySelector(".actualPercent")
 let actualContentPercent = document.querySelector(".actualPercent")
 let levelPercent = document.querySelector(".progressingPercent")
 let remain = document.querySelector(".remainPercent")
 
-//Color blend
+//Color blend of the jar
 let colorFillin = ["#a5f2bf","#d98d3d","d9713d","f28dd5","#b84d6a",'#e3cad6',"#b7caf7","#a2f5f3"]
 
-// function randomColorJar() {
-//     let rainbow = Math.floor(Math.random()* colorFillin.length) //getting the index but not the value
-//     return colorFillin[rainbow]
-// }
-
-
-//Addlistener : adding/removing with click 
+//Addlistener : adding/removing with click. For each button, we'll do an listener of events taht with call the function (adding or removing created below)
 allbuttonAdding.forEach(element =>{
     element.addEventListener('click', (adding))
     
@@ -54,27 +45,22 @@ function adding() {
     //Actual fill in
     actualContentCup.textContent = `${cupIngredient/8}`;
     actualContentPercent.textContent = `${percentIngredient}%`;
-    levelPercent.style.height = `${percentIngredient}%` //assigner la hauteur de la jar de progression au pourcentage ajouté (100% = jar complète)
-    remain.innerText = (`${left}` - `${percentIngredient}`)+ `%`
-    
-    console.log(cupIngredient,percentIngredient);
-    console.log(remain.innerText);
+    levelPercent.style.height = `${percentIngredient}%` //assigning the height of the jar related to the percentage added
+    remain.innerText = (`${left}` - `${percentIngredient}`)+ `%` //variable that will display the remained content of the jar
 
-    if (cupIngredient === maxCup) { //stop l'action du bouton(rendu inactif) quand on a atteint le max
+    if (cupIngredient === maxCup) { // condition that will stop the action of each button if the content reached the max cup. With the disabled property will make the button unclickable : it's what we want for not going above 100%
         allbuttonAdding.forEach(element => {
             element.disabled = true
         });
-        levelPercent.style.width="99%"
-        // levelPercent.innerText="Ready to drink"
-        levelPercent.style.backgroundImage="linear-gradient(to top,rgb(123, 211, 234), rgb(255, 155, 210))"
+        //Styling the progression in the gar when the cup is full
+        levelPercent.style.backgroundImage="linear-gradient(to top,rgb(123, 211, 234), rgb(235, 75, 163))"
         actualContentPercent.textContent=""
         remain.innerText="100% ready to drink !"
+        remain.style.textAlign="center"
         remain.style.fontFamily="Arial"
-        remain.style.fontSize="20px"
+        remain.style.fontSize="30px"
         remain.style.color="white"
         remain.style.paddingTop="40%"
-        remain.style.fontFamily="Josefin Sans', sans-serif"
-
     
     } else {
         allbuttonRemoving.forEach(element => {
@@ -90,16 +76,13 @@ function removing() {
     //Removing fill in
     actualContentCup.textContent = `${cupIngredient/8}`;
     actualContentPercent.textContent = `${percentIngredient}%`;
-    levelPercent.style.height = `${percentIngredient}%` //assigner la hauteur de la jar de progression au pourcentage ajouté (100% = jar complète)
+    levelPercent.style.height = `${percentIngredient}%`
     remain.textContent = `${left}` - `${percentIngredient}`+ `%`
 
-    console.log(cupIngredient,percentIngredient);
-
-    if (cupIngredient == minCup) { //stop l'action du bouton(rendu inactif) quand on a atteint le max
+    if (cupIngredient == minCup) { // condition that will stop the action of each button if the content is in the min level. With the disabled property will make the button unclickable : it's what we want for not going under 0%
         allbuttonRemoving.forEach(element => {
             element.disabled = true
         });
-        // allbuttonRemoving.disabled = true //mettre le bouton en inactif dans le HTML : par défaut on ne peut retirer quelque chose si on a rien de base !!!
         
     } else {
         allbuttonAdding.forEach(element => {
